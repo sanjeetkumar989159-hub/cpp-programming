6d0e366c7c230b69dc5861fe1e93233839c986a7
sanjeet kumar <sumitkumar989159@gmail.com>
2026-03-22 20:55:44 +0530

    Add files via upload

---
+++ basic code c++/sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int a,b,sum=0;
+     cout<<"enter the value of a and b =";
+     cin>>a>>b;
+    sum=a+b;
+    cout<<"the sum of a and b is "<<sum;
+     return 0;
+
+};
