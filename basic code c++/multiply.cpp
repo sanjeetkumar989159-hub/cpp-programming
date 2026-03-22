f8da168efb7ec9009d0b3923b21716c9f25f598f
sanjeet kumar <sumitkumar989159@gmail.com>
2026-03-22 21:01:34 +0530

    Add files via upload

---
+++ basic code c++/multiply.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,mul=0;
+    cout<<"enter the value of a and b =";
+    cin>>a>>b;
+    mul=a*b;
+    cout<<"the multiplication of a and b is "<<mul;
+    return 0;
+}
