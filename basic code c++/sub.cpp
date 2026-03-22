2d37d39be0f51906120a6a286d51c2dc51bc6fbb
sanjeet kumar <sumitkumar989159@gmail.com>
2026-03-22 20:58:12 +0530

    Add files via upload

---
+++ basic code c++/sub.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sub=0;
+    cout<<"enter the value of a and b =";
+    cin>>a>>b;
+    sub=a-b;        
+    cout<<"the subtraction of a and b is "<<sub;
+     return 0;
+}
