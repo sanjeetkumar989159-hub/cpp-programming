01849a149b8f3e816f9c07770cc490fe7730a87c
sanjeet kumar <sumitkumar989159@gmail.com>
2026-04-13 21:57:01 +0530

    Add files via upload

---
+++ Pattern printing/number3.cpp
@@ -0,0 +1,19 @@
+//5  4  3  2  1  
+//5  4  3  2  1  
+//5  4  3  2  1  
+//5  4  3  2  1  
+//5  4  3  2  1  
+#include<iostream>
+using namespace std;
+int main()
+{
+     int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=5;j>=1;j--)
+    {
+    cout<<j<<"  ";
+} 
+cout<<endl;
+    }
+}
