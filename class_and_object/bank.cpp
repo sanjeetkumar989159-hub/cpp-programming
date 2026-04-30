dc9a277781a3f33e79f9d06ca751eb5183652839
sanjeet kumar <sumitkumar989159@gmail.com>
2026-04-30 22:55:55 +0530

    Add files via upload

---
+++ class_and_object/bank.cpp
@@ -0,0 +1,31 @@
+
+//WAP to fetch bank details with credit a nd debit amount by using class and object there must be ensure that there must be a constructor
+ #include<iostream>
+using namespace std;
+
+class Bank {
+public:
+    string name;
+    int balance;
+
+    
+    Bank() {
+        name = "sanjeet";
+        balance = 1000;
+
+    
+        balance = balance + 500;
+
+        
+        balance = balance - 200;
+
+        
+        cout << "Name: " << name << endl;
+        cout << "Final Balance= " << balance << endl;
+    }
+};
+
+int main() {
+    Bank b1;  
+    return 0;
+}
