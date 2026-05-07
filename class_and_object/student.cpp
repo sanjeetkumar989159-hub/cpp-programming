c87fc5863d4247a3e8f0abff56c10f52490d28f3
sanjeet kumar <sumitkumar989159@gmail.com>
2026-05-07 23:23:05 +0530

    Add files via upload

---
+++ class_and_object/student.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+class student
+{
+    protected:
+    int roll;
+    string name;
+    public:
+     void studentdata ()
+{
+    cout<<"Enter the roll number : "<<endl;
+    cin>>roll;
+    cout<<"enter the name of student :";
+    cin>>name;
+
+}
+     
+};
+ class result:public student
+ {
+    private:
+    int m1,m2,m3;
+    float percentage;
+    char grade;
+    public:
+    void getresult()
+    {
+    cout<<"Enter the value of three subject :"<<endl;
+    cin>>m1>>m2>>m3;
+    percentage = ((m1+m2+m3) * 100.0) / 300;
+     if (percentage>80)
+     grade='A';
+
+     else if (percentage >60)
+     grade='B';
+     else if(percentage>45)
+     grade='c';
+     else
+     grade='D';
+
+    }
+     void display()
+     {
+        cout<<"Roll number :"<<roll<<endl;
+        cout<<"Name is:"<<name<<endl;
+        
+        cout<<"percentage:"<<percentage<<endl;
+    
+        cout<<"grade is:"<<grade<<endl;
+        
+        
+     }
+      
+ };
+ int main()
+ {
+    result s;
+    s.studentdata();
+    s.getresult();
+    s.display();
+ }
