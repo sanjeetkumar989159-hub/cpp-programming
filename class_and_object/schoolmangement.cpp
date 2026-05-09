192e264d783f02f5bc3b9411189f82e9f1c9cf35
sanjeet kumar <sumitkumar989159@gmail.com>
2026-05-09 21:40:55 +0530

    Add files via upload

---
+++ class_and_object/schoolmangement.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+class Person
+{
+protected:
+    string name;
+
+public:
+    void getPerson()
+    {
+        cout<<"Enter Name: ";
+        cin>>name;
+    }
+};
+
+class Student : public Person
+{
+protected:
+    int marks;
+
+public:
+    void getMarks()
+    {
+        cout<<"Enter Marks: ";
+        cin>>marks;
+    }
+};
+
+class SportsStudent : public Student
+{
+protected:
+    int sportsMarks;
+
+public:
+    void getSports()
+    {
+        cout<<"Enter Sports Marks: ";
+        cin>>sportsMarks;
+    }
+};
+
+class Result : public SportsStudent
+{
+private:
+    int total;
+
+public:
+    void calculate()
+    {
+        total = marks + sportsMarks;
+    }
+
+    void display()
+    {
+        cout<<"\nName: "<<name;
+        cout<<"\nAcademic Marks: "<<marks;
+        cout<<"\nSports Marks: "<<sportsMarks;
+        cout<<"\nTotal: "<<total;
+    }
+};
+
+int main()
+{
+    Result r;
+
+    r.getPerson();
+    r.getMarks();
+    r.getSports();
+
+    r.calculate();
+    r.display();
+
+    return 0;
+}
