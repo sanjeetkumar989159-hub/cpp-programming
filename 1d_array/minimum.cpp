fb074a81dfa8377633945a61e4250b8783ba04ac
sanjeet kumar <sumitkumar989159@gmail.com>
2026-06-06 22:47:28 +0530

    Add files via upload

---
+++ 1d_array/minimum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    cout << "Minimum element = " << min << endl;
+
+    return 0;
+}
