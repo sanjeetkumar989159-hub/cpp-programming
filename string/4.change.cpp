e73a9f51a52cfd0c618f035b4018c2587ef9a842
sanjeet kumar <sumitkumar989159@gmail.com>
2026-07-01 23:36:23 +0530

    Add files via upload

---
+++ string/4.change.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // Change upper case to lower case without using built in function 
+
+    string str;
+    cout<<"Enter your sentence :"<<endl;
+
+    getline(cin,str);
+
+    for(int i = 0;str[i] !='\0';i++){
+        if(str[i] < 97 && str[i] != ' '){
+            str[i] = str[i] + 32;
+        }
+        else if(str[i] == ' '){
+            str[i] = ' ';
+        }
+        cout<<str[i];
+    }
+
+    return 0;
+}
